c1af363a6d055747595a08b018f44740ceb560c6
杜文斌 <857617704@qq.com>
2024-07-04 20:13:45 +0800

    新增:prettier

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,62 +1,54 @@
 <script setup lang="ts">
 import { ref, reactive, toRefs, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useCounterStore } from '@/stores/counter';
-import { useSystemStore } from '@/stores/system';
+import { useCounterStore } from '@/stores/counter'
+import { useSystemStore } from '@/stores/system'
 import * as apiHome from '@/api/home.js'
 
 const { locale } = useI18n()
 const useSystem = useSystemStore()
 //防止结构后丢失响应性
 const { count, addCount } = toRefs(useCounterStore())
-const showPopover = ref(false);
+const showPopover = ref(false)
 
 // 通过 actions 属性来定义菜单选项
 const actions = [
-    { text: '中文', lang: 'zh_Hans' },
-    { text: 'English', lang: 'en' }
-];
+  { text: '中文', lang: 'zh_Hans' },
+  { text: 'English', lang: 'en' },
+]
 
 const onSelect = (action: any) => {
-    changeLang(action.lang)
-};
-
+  changeLang(action.lang)
+}
 
 const changeLang = (lang: string) => {
-    locale.value = lang
-    useSystem.changeLang(lang)
+  locale.value = lang
+  useSystem.changeLang(lang)
 }
-const init =async()=>{
-await apiHome.getLayout()
+const init = async () => {
+  await apiHome.getLayout()
 }
-onMounted(()=>{
-    init()
+onMounted(() => {
+  init()
 })
-
-
 </script>
 
 <template>
-    <div class="page p-[20px]">
-        <div class="h-[40px]  w-full">
-            {{ $t('hello') }}
-        </div>
-        <div class="h-[40px]  w-full">
-            {{ $t('welcome', { x: 'Q仔' }) }}
-        </div>
-        <div class="mt-[20px] flex items-center">
-            <span class="w-[200px] flex">{{ count }}</span>
-            <van-button @click="addCount()" type="primary">
-                Add
-            </van-button>
-        </div>
-        <van-popover v-model:show="showPopover" :actions="actions" @select="onSelect">
-            <template #reference>
-                <van-button type="primary">{{ $t('change') }}</van-button>
-            </template>
-        </van-popover>
-
-
-
+  <div class="page p-[20px]">
+    <div class="h-[40px] w-full">
+      {{ $t('hello') }}
     </div>
+    <div class="h-[40px] w-full">
+      {{ $t('welcome', { x: 'Q仔' }) }}
+    </div>
+    <div class="mt-[20px] flex items-center">
+      <span class="w-[200px] flex">{{ count }}</span>
+      <van-button @click="addCount()" type="primary">Add</van-button>
+    </div>
+    <van-popover v-model:show="showPopover" :actions="actions" @select="onSelect">
+      <template #reference>
+        <van-button type="primary">{{ $t('change') }}</van-button>
+      </template>
+    </van-popover>
+  </div>
 </template>
